8474e10ded98294a7dd2ee609092ac5e532b0c45
Fomin Sergey <gray7410@yandex.ru>
2022-04-23 09:08:09 +0500

    add event sort

--- src/App/components/usersList.jsx
+++ src/App/components/usersList.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Pagination from "./pagination";
-import User from "./user";
 import PropTypes from "prop-types";
 import { paginate } from "../utils/paginate";
 import GroupList from "./groupList";
 import api from "../api";
 import SearchStatus from "./searchStatus";
+import UserTable from "./usersTable";
+import _ from "lodash";
 
 const UsersList = ({ users: allUsers, onDelete }) => {
-    const pageSize = 2;
+    const pageSize = 8;
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions] = useState(api.professions.fetchAll());
     const [selectedProf, setSelectedProf] = useState();
     const [profLoading, setProfLoading] = useState(true);
+    const [sortBy, setSortBy] = useState({ iter: "name", order: "asc" });
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
     };
@@ -28,15 +30,18 @@ const UsersList = ({ users: allUsers, onDelete }) => {
     const handleProfessionSelect = (item) => {
         setSelectedProf(item);
     };
+    const handleSort = (item) => {
+        setSortBy({ iter: item, order: "asc" });
+    };
 
     const filteredUsers = selectedProf
         ? allUsers.filter((user) => user.profession._id === selectedProf._id)
         : allUsers;
     const count = filteredUsers.length;
-
-    const userCrop = paginate(filteredUsers, currentPage, pageSize);
+    const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order]);
+    const userCrop = paginate(sortedUsers, currentPage, pageSize);
     useEffect(() => {
-        if (!userCrop.length && filteredUsers) {
+        if (!userCrop.length && count) {
             setCurrentPage((page) => page - 1);
         }
     }, [onDelete]);
@@ -63,29 +68,12 @@ const UsersList = ({ users: allUsers, onDelete }) => {
             )}
             <div className="d-flex flex-column flex-grow-1 p-3">
                 <SearchStatus users={count} selectedProf={selectedProf} />
-                {count > 0 && (
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Имя</th>
-                                <th scope="col">Качества</th>
-                                <th scope="col">Профессия</th>
-                                <th scope="col">Встретился, раз</th>
-                                <th scope="col">Оценка</th>
-                                <th scope="col">Избранное</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {userCrop.map((user) => (
-                                <User
-                                    key={user._id}
-                                    onDelete={onDelete}
-                                    {...user}
-                                />
-                            ))}
-                        </tbody>
-                    </table>
+                {count && (
+                    <UserTable
+                        users={userCrop}
+                        onDelete={onDelete}
+                        onSort={handleSort}
+                    />
                 )}
                 <div className="d-flex justify-content-center">
                     <Pagination
---
+++ src/App/components/usersTable.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import User from "./user";
+
+const UserTable = ({ users, onDelete, onSort }) => {
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th onClick={() => onSort("name")} scope="col">
+                        Имя
+                    </th>
+                    <th scope="col">Качества</th>
+                    <th onClick={() => onSort("profession.name")} scope="col">
+                        Профессия
+                    </th>
+                    <th onClick={() => onSort("completedMeetings")} scope="col">
+                        Встретился, раз
+                    </th>
+                    <th onClick={() => onSort("rate")} scope="col">
+                        Оценка
+                    </th>
+                    <th onClick={() => onSort("bookmark")} scope="col">
+                        Избранное
+                    </th>
+                    <th />
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                    <User key={user._id} onDelete={onDelete} {...user} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+UserTable.propTypes = {
+    users: PropTypes.array.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onSort: PropTypes.func.isRequired
+};
+
+export default UserTable;
