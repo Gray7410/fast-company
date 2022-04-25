9ce806751b8c0741aaee44e85aec34dd93e29719
Fomin Sergey <gray7410@yandex.ru>
2022-04-25 15:23:41 +0500

    ext tableBody

--- src/App/components/usersTable.jsx
+++ src/App/components/usersTable.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import User from "./user";
 import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
 
 const UserTable = ({ users, onDelete, selectedSort, onSort }) => {
     const columns = {
-        name: { iter: "name", name: "Имя" },
+        name: { path: "name", name: "Имя" },
         qualities: { name: "Качетва" },
-        professions: { iter: "profession.name", name: "Профессия" },
+        professions: { path: "profession.name", name: "Профессия" },
         completedMeetings: {
-            iter: "completedMeetings",
+            path: "completedMeetings",
             name: "Встретился раз"
         },
-        rate: { iter: "rate", name: "Оценка" },
-        bookmark: { iter: "bookmark", name: "Избранное" },
+        rate: { path: "rate", name: "Оценка" },
+        bookmark: { path: "bookmark", name: "Избранное" },
         delete: {}
     };
     return (
         <table className="table">
             <TableHeader {...{ onSort, selectedSort, columns }} />
-            <tbody>
-                {users.map((user) => (
-                    <User key={user._id} onDelete={onDelete} {...user} />
-                ))}
-            </tbody>
+            <TableBody {...{ columns, data: users }} />
         </table>
     );
 };
