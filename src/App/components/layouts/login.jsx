7d9ab5206c87e307a00d65fa7d13de1ea0eae770
Sergey Fomin <gray7410@yandex.ru>
2022-05-04 17:14:21 +0500

    submit

--- src/App/components/layouts/login.jsx
+++ src/App/components/layouts/login.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "../textField";
 
 const Login = () => {
-    return <h1>Авторизация</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(data);
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Почта"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+            />
+            <TextField
+                label="Пароль"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+            />
+            <button>Войти</button>
+        </form>
+    );
 };
 
 export default Login;
